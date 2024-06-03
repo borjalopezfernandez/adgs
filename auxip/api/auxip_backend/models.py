06e343219a7fb47a7a4f9ef5917711fffc46aec9
Borja Lopez Fernandez <borja.lopez@deimos-space.com>
2024-06-03 19:04:49 +0200

    AUXIP model for notifications of new products implemented with simulated end-point Updated: > test_unit > test_integration

--- auxip/api/auxip_backend/models.py
+++ auxip/api/auxip_backend/models.py
@@ -1,7 +1,6 @@
 # Create SQLAlchemy models
-# https://fastapi.tiangolo.com/tutorial/sql-databases/
-# https://pypi.org/project/sqlalchemy-guid/
-# https://www.getorchestra.io/guides/fastapi-and-datetime-types-a-comprehensive-guide
+
+# https://www.adamsmith.haus/python/docs/sqlalchemy.dialects.postgresql.ENUM
 
 import datetime
 import enum
@@ -24,14 +23,6 @@ from sqlalchemy_guid import GUID
 
 from .database import Base
 
-
-class Product(Base):
-    __tablename__   = "archived_files"
-    Name            = Column(String(255), primary_key=True, unique=False)
-    ContentType     = Column(String(63), unique=False)
-    ContentLength   = Column(Integer, nullable=False)
-
-
 class EnumSubscriptionStatus(str, enum.Enum):
     running     = 0
     paused      = 1
@@ -39,7 +30,7 @@ class EnumSubscriptionStatus(str, enum.Enum):
 
 
 class Subscription(Base):
-    __tablename__ = "subscriptions"
+    __tablename__ = "subscription"
     Id                              = Column(Uuid,          primary_key = True, default=uuid.uuid4)
     Status: EnumSubscriptionStatus  = Column(Enum(EnumSubscriptionStatus))
     FilterParam                     = Column(String(512),   unique = False)
@@ -52,3 +43,21 @@ class Subscription(Base):
     def __repr__(self):
         return f"Id:{self.Id} - Status:{self.Status}"
 
+
+class SubscriptionNotification(Base):
+    __tablename__ = "subscription_notification"
+    ProductId                       = Column(Uuid)
+    SubscriptionId                  = Column(Uuid)
+    ProductName                     = Column(String(255),   unique = False)
+    NotificationDate                = Column(DateTime,      nullable = False)
+    # extended attributes not part of ESA-EOPG-EOPGC-IF-10 Issue 1.5 Date 26/04/2023
+    id                              = Column(Integer,       primary_key = True, autoincrement = True)
+    NotificationSuccess             = Column(Boolean,       nullable = False)
+    NotificationInfo                = Column(String(512),   unique = False)
+
+
+class Product(Base):
+    __tablename__   = "archived_files"
+    Name            = Column(String(255), primary_key=True, unique=False)
+    ContentType     = Column(String(63), unique=False)
+    ContentLength   = Column(Integer, nullable=False)
---
+++ auxip/api/test/main_test_integration.py
@@ -0,0 +1,200 @@
+import sys
+import pytest
+import datetime
+import requests
+import logging as logger
+import json
+from uuid import UUID
+
+from starlette.testclient import TestClient
+
+from auxip_backend import database, models, schemas, crud
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+
+from auxip_backend.main import app
+
+subscription_id             = None
+notification_end_point      = "http://localhost:8000/test/EP/Notification/ProductAvailability"
+notification_end_point_unav = "http://cannotreachme:1234/test/EP/Notification/ProductAvailability"
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://adgs:adg$#5432@127.0.0.1/adgs_db"
+engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal            = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db                      = SessionLocal()
+   
+
+@pytest.fixture(scope = "module", autouse = True)
+def init_db():
+    print()
+    print('init_db() start')
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+    print('init_db() end')
+   
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+
+@pytest.fixture
+def print_separator():
+    print("\n=========================================================================")
+
+
+
+def test_subscription_cycle(client, print_separator):
+    """
+    GIVEN some subscription input
+    WHEN posting the subscription input
+    THEN response with status 200
+    """
+    print(f"START : {sys._getframe().f_code.co_name}")
+    
+    print("POST /odata/v1/Subscription")
+    
+    subs = {
+        "FilterParam": "contains(Name,'AUX_UT1UTC')",
+        "NotificationEndpoint": notification_end_point,
+        "NotificationEpUsername": "",
+        "NotificationEpPassword": "",
+        "Status": "0"
+    }
+
+    response = client.post("/odata/v1/Subscription", json=subs)
+    assert response.status_code == 201
+    json_data = response.json()
+    print(json_data)
+
+    id              = json_data['Id']
+    subscription_id = id
+    subs_id         = {'Id' : id}
+    json_subs_id    = json.dumps(subs_id)
+    print(json_subs_id)
+
+    url = f"/odata/v1/Subscription/{id}"
+    print(f"GET {url}")
+
+    response = client.get(url)
+    assert response.status_code == 200
+    print(response.headers)
+
+    subs_update      = {'Id' : id, 'Status' : '0' }
+    print(subs_update)
+
+    print("PUT /odata/v1/Subscription/Status")
+    response = client.put("/odata/v1/Subscription/Status", json=subs_update)
+    print(response)
+    assert response.status_code == 200
+
+    subs_update      = {'Id' : id, 'Status' : '0' }
+    print()
+    print(subs_update)
+
+    print(f"END : {sys._getframe().f_code.co_name}")
+
+
+
+def test_subscription_notification(client, print_separator):
+    """
+    GIVEN some subscription input
+    WHEN posting the subscription input
+    THEN response with status 200
+    """
+    print(f"START : {sys._getframe().f_code.co_name}")
+    
+    print("GET /odata/v1/Subscriptions/Id")
+    
+    response = client.get("/odata/v1/Subscriptions/Id")
+    assert response.status_code == 200
+    json_data = response.json()
+
+    subscription_id = json_data[0]['Id']
+    print(subscription_id)
+    
+    now     = datetime.datetime.utcnow()
+    str_now = now.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+
+    subscription_product_notification = {
+        "NotificationDate": str_now,
+        "ProductId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "ProductName": "S2__OPER_AUX_UT1UTC_PDMC_20240513T000000_V20170101T000000_21000101T000000.7z",
+        "SubscriptionId": subscription_id
+    }
+
+    print(f"POST {notification_end_point}")
+    response = client.post(notification_end_point, json=subscription_product_notification)
+    assert response.status_code == 200
+    json_data = response.json()
+    print(json_data)
+
+    notification_success  = schemas.SubscriptionNotificationDB(NotificationSuccess = True,
+                                                                NotificationInfo   = str(json_data),
+                                                                ProductId           = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                                                                ProductName         = "S2__OPER_AUX_UT1UTC_PDMC_20240513T000000_V20170101T000000_21000101T000000.7z",
+                                                                Success             = False,
+                                                                SubscriptionId      = UUID(subscription_id),
+                                                                NotificationDate    = now
+                                                                )
+    print("after pydantic")
+    crud.create_subscription_notification_product(db = db, notification = notification_success)
+
+    print(f"END : {sys._getframe().f_code.co_name}")
+
+
+
+def test_subscription_notification_failure(client, print_separator):
+    """
+    GIVEN some subscription product notification
+    WHEN posting to some unavailable / unreachable EP
+    THEN response with status != 200
+    """
+    print(f"START : {sys._getframe().f_code.co_name}")
+    
+    print("GET /odata/v1/Subscriptions/Id")
+    
+    response = client.get("/odata/v1/Subscriptions/Id")
+    assert response.status_code == 200
+    json_data = response.json()
+
+    subscription_id = json_data[0]['Id']
+    print(subscription_id)
+    
+    now     = datetime.datetime.utcnow()
+    str_now = now.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+    subscription_product_notification = {
+        "NotificationDate": str_now,
+        "ProductId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "ProductName": "S2__OPER_AUX_UT1UTC_PDMC_20240513T000000_V20170101T000000_21000101T000000.7z",
+        "SubscriptionId": subscription_id
+    }
+
+    print(subscription_product_notification)
+
+    print(f"POST {notification_end_point_unav}")
+
+    try:
+        post_response = requests.post(notification_end_point_unav, json = subscription_product_notification)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("before pydantic")
+        notification_error  = schemas.SubscriptionNotificationDB(NotificationSuccess = False,
+                                                                NotificationInfo   = str(err),
+                                                                ProductId           = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                                                                ProductName         = "S2__OPER_AUX_UT1UTC_PDMC_20240513T000000_V20170101T000000_21000101T000000.7z",
+                                                                SubscriptionId      = UUID(subscription_id),
+                                                                NotificationDate    = now
+                                                                )
+        print("after pydantic")
+        crud.create_subscription_notification_product(db = db, notification = notification_error)
+
+
+    print(f"END : {sys._getframe().f_code.co_name}")
